893b09ca673465a0c4b89130c6cae1774376a50a
Nguyen Quoc Phong <quocphongg2003@gmail.com>
2024-04-01 12:05:01 +0700

    Config router/layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            let Layout = DefaultLayout;
+
+            if (route.layout === null) {
+              Layout = Fragment;
+            } else if (route.layout) {
+              Layout = route.layout;
+            }
+
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
