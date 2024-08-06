7641004707f8f8edbe6ca5e1f2d3cfcc3f1d9a9e
Nguyen Quoc Phong <quocphongg2003@gmail.com>
2024-08-06 20:04:55 +0700

    Final of final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FeatureUnavailable } from './components/FeatureUnavailable';
 
 function App() {
@@ -17,6 +17,18 @@ function App() {
       setNoSupport(false);
     }
   };
+
+  useEffect(() => {
+    const width = window.innerWidth;
+
+    if (width <= 739) {
+      setNoSupport(true);
+    } else if (width >= 740 && width <= 1024) {
+      setNoSupport(true);
+    } else if (width >= 1025) {
+      setNoSupport(false);
+    }
+  }, []);
 
   return !noSupport ? (
     <div className="App grid">
